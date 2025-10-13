8d0fdb4406d9cce947953677c248c101502e4614
amusl <Amuslimov050@gmail.com>
2025-10-13 20:45:56 +0600

    add class userService

---
+++ src/UserService.java
@@ -0,0 +1,15 @@
+public class UserService {
+    private User user;
+
+    public UserService(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
