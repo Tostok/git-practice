bd3c082227b30a863b2596e1eda2dab0855ab4c4
amusl <Amuslimov050@gmail.com>
2025-10-13 20:57:21 +0600

    add repository for users

---
+++ src/userRepository.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class userRepository {
+    private List<User> users;
+
+    public userRepository(List<User> users) {
+        this.users = users;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
