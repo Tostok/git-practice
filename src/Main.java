abf9e2d913916adb6719746babc157c5afa323b0
Amir Muslimov <160594950+Tostok@users.noreply.github.com>
2025-10-13 21:47:23 +0600

    Update Main.java
    добавил доп. sout

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("привет гит!");
+        System.out.println("Привет гит!"); 
+        System.out.println("До свидания гит!")
     }
 }
